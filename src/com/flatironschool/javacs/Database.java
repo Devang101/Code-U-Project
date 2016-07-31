f6fd00d62fcd727879b1c79e38ef9fa12a4cc5a1
“Opoku-Agyemanggit config --global user.email “samuel.opokuagyemang@gmail.com <samuel.opokuagyemang@gmail.com>
2016-07-31 19:37:18 -0400

    Database Updates

--- src/com/flatironschool/javacs/Database.java
+++ src/com/flatironschool/javacs/Database.java
@@ -1,8 +1,10 @@
-package com.flatironschool.javacs;
-
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public  class Database {
@@ -11,6 +13,44 @@ public  class Database {
     public static int ID = 0;
     public static int count = 0;
     
+    
+    public static void populateMasterDB(){
+        masterDB = new HashMap<String, HashMap<Integer, Integer>>();
+        String csvFile = "MasterDB.csv";
+        String line = "";
+        String csvSplitBy = ",";
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            
+            while ((line = br.readLine()) != null) {
+                
+                // use comma as separator
+                String[] array = line.split(csvSplitBy);
+                
+                masterDB.put(array[0], new HashMap<Integer, Integer>());
+                for(int i=1; i<array.length;i++){
+                    masterDB.get(array[0]).put(extractID(array[i]), extractRelevancy(array[i]));
+                }
+                
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void printDB(){
+        System.out.println(masterDB.toString());
+        
+    }
+    
+    public static Integer extractID(String s){
+        return Integer.valueOf(s.substring(0,s.indexOf('-')));
+    }
+    
+    public static Integer extractRelevancy(String s){
+        return Integer.valueOf(s.substring(s.indexOf('-')+1));
+    }
     
     
     public static void exportMasterDBToCSV(){
@@ -72,18 +112,20 @@ public  class Database {
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        masterDB = new HashMap<String, HashMap<Integer, Integer>>();
-        for(int i=0; i<21;i++){
-            masterDB.put("word"+i, new HashMap<Integer, Integer>());
-            masterDB.get("word"+i).put(i, i);
-            masterDB.get("word"+i).put(i+2, i+2);
-            masterDB.get("word"+i).put(i+3, i+3);
-            masterDB.get("word"+i).put(i+4, i+4);
-            masterDB.get("word"+i).put(i+5, i+5);
-        }
-        
-        System.out.println("about to");
-        exportMasterDBToCSV();
+        /*		masterDB = new HashMap<String, HashMap<Integer, Integer>>();
+         for(int i=0; i<21;i++){
+         masterDB.put("word"+i, new HashMap<Integer, Integer>());
+         masterDB.get("word"+i).put(i, i);
+         masterDB.get("word"+i).put(i+2, i+2);
+         masterDB.get("word"+i).put(i+3, i+3);
+         masterDB.get("word"+i).put(i+4, i+4);
+         masterDB.get("word"+i).put(i+5, i+5);
+         }
+         
+         System.out.println("about to");
+         exportMasterDBToCSV();*/
+        populateMasterDB();
+        printDB();
         
     }
     
